af3e463a83393ae11c66b88834e7d3b02491655e
Ebrahim Amin <borcomton@gmail.com>
2024-12-11 22:39:49 +0200

    Final Edit on Project , manual team tasks merge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -53,26 +52,23 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.androidx.room.runtime)
-    annotationProcessor(libs.androidx.room.compiler)
+    kapt(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
-    implementation (libs.retrofit)
-    implementation (libs.converter.gson)
-    implementation (libs.gson)
-    implementation ("androidx.work:work-runtime-ktx:2.7.1")
-
-    implementation (libs.glide)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.gson)
+    implementation("androidx.work:work-runtime-ktx:2.7.1")
+    implementation(libs.glide)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.savedstate)
     kapt(libs.androidx.lifecycle.compiler)
-    implementation (libs.androidx.navigation.fragment.ktx)
-    implementation (libs.androidx.navigation.ui.ktx)
-    implementation (libs.kotlinx.coroutines.android)
-    val room_version = "2.6.1"
-    kapt("androidx.room:room-compiler:$room_version")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.2")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.2")
-    implementation("com.airbnb.android:lottie:4.2.2")
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx.v272)
+    implementation(libs.androidx.lifecycle.livedata.ktx.v272)
+    implementation(libs.lottie)
 }
